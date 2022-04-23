203eaa1d40eb603cc96329b223577f2dfea683a3
Websitio <webica@hotmail.com>
2022-04-23 19:33:07 +0100

    Controller 1st step TDD & Swagger

---
+++ server.tests/CostControllerTests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+// using server.Controllers;
+
+namespace server.tests
+{
+    [TestClass]
+    public class CostControllerTests
+    {
+        [TestMethod]
+        public void GetAll()
+        {
+            var controller = new CostController();
+
+            var result = controller.GetAll();
+
+            Assert.IsNotNull(result);
+      }
+    }
+}
---
+++ server/Controllers/CostController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace server
+{
+
+    [Route("Putin_Is_A_Serial_Killer")]
+    public class CostController : Controller
+    {
+          [HttpGet()]
+          public JsonResult  GetAll(){
+                return new JsonResult(new List<object>()
+                {
+                    new {Id=1,UserId=1,Amount=10},
+                    new {Id=2,UserId=1,Amount=20},
+                    new {Id=3,UserId=1,Amount=30}
+
+                });
+
+            }
+
+        }
+    }
