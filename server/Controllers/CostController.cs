829e6bb6c323f14c726c2db0336ea4aa576a7523
Websitio <webica@hotmail.com>
2022-05-23 22:02:39 +0100

    Simplest - all endpoints in one class ready for refactoring

--- server/Controllers/CostController.cs
+++ server/Controllers/CostController.cs
@@ -8,6 +8,7 @@ namespace server
     [Route("Putin_Is_A_Serial_Killer")]
     public class CostController : Controller
     {
+       
           [HttpGet()]
           public JsonResult  GetAll(){
                 return new JsonResult(new List<object>()
@@ -18,9 +19,9 @@ namespace server
                 });
 
             }
-
-
-         [HttpGet()]
+       
+       /* */
+         [HttpGet("users")] 
          public JsonResult GetAllUsers()
         {
             return new JsonResult(new List<object>()
@@ -29,21 +30,42 @@ namespace server
                     new {Id=2,UserName="Janet"}
          });
 
-        }
-     
-        [HttpGet()]
-        public JsonResult GetAllCosts()
+        }    //End of GetAllUsers
+
+
+
+
+
+
+
+
+        /*
+          [HttpGet()] 
+          public JsonResult GetAllCosts()
+          {
+              return new JsonResult(new List<object>()
+                  {
+                      new {Id=1,UserId=1, Amount=4},
+                      new {Id=2,UserId=1, Amount=3 },
+                      new {Id=3,UserId=2, Amount=2 },
+                      new {Id=4,UserId=2, Amount=1 },
+
+                  });
+                }   //End of GetAllCosts
+
+         */
+
+
+        [HttpGet("categories")]
+        public JsonResult GetAllCategories()
         {
             return new JsonResult(new List<object>()
                 {
-                    new {Id=1,UserId=1, Amount=4},
-                    new {Id=2,UserId=1, Amount=3 },
-                    new {Id=3,UserId=2, Amount=2 },
-                    new {Id=4,UserId=2, Amount=1 },
+                    new {Id=1,Name="Petty Cash"},
+                    new {Id=2,UserName="Milk"}
+         });
 
-                });
-
-        }
+        }    //End of GetAllUsers
 
 
 
